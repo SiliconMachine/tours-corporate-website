0b93d68bd5fed49f9b506e09bad23d2a0c8bec6b
Alan <mmckalan@gmail.com>
2020-05-24 16:07:23 +0200

    First commit

--- tours/scripts.js
+++ tours/scripts.js
@@ -1,1 +1,16 @@
-//start
+// To select elements
+
+const selectElement = (s) => document.querySelector(s)
+
+
+// Open the menu on click
+selectElement('.open').addEventListener('click', () => {
+    console.log('opening')
+    selectElement('.nav-list').classList.add('active');
+});
+
+// Close the menu on click
+
+selectElement('.close').addEventListener('click', () => {
+    selectElement('.nav-list').classList.remove('active');
+})
